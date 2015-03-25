6edc2a065abbba3706a89ac0a3818916b4151660
hojene007 <yevgeniy.levin@barcelonagse.eu>
2015-03-25 13:32:28 +0100

    fun fun

--- code/svm/funStuffSVM.R
+++ code/svm/funStuffSVM.R
@@ -1,3 +1,16 @@
+# In this file some results are sumarised in graphs on interesting variations of the models
+# tested in svmTry2.R # also it includes support functions
+
+install.packages("ggplot2")
+library("ggplot2")
+#function for cross validation
+crossVal <- function(trueClass, predV) {
+  # error for support vector machines
+  a <- sum(sign(trueClass)==sign(predV))/length(predV)
+  return (1-a)
+}
+
+# lets make a plot of how error increases with C 
 
 cVec <- seq(1, 1000, 50)
 errVec <- rep(2, length(cVec))
@@ -14,8 +27,9 @@ errDF <- data.frame(cVec, errVec, crossValVec)
 colnames(errDF) <- c("C", "InSError", "CrossValErr")
 
 ggplot(data = errDF, aes(x = C)) + 
-  geom_line(aes(y=InSError, colour="red")) +
-  geom_line(aes(y=CrossValErr, colour="blue")) +
-  xlab("Height") +
-  ylab("Weight") +
-  theme_bw(base_size = 14, base_family = "Helvetica")
+  geom_line(aes(y=InSError, colour="Error - Insample")) +
+  geom_line(aes(y=CrossValErr, colour="Error - 10xCross Val.")) +
+  xlab("C-parameter") +
+  ylab("Error Rate") +
+  theme_bw(base_size = 14, base_family = "Helvetica")+
+  ggtitle("C parameter vs errors")
