b51b620dbd852091851eff9d9d189a5d374a02c2
JordiZamora <jordi.zamora@barcelonagse.eu>
2015-03-27 19:36:25 +0100

    Revised final code

---
+++ Machine-LearningDLZ/Final_classifier/code/CV_Prarallel_kNN.R
@@ -0,0 +1,55 @@
+Misclass1NN <- function(weights,rawdata, cores, k=1){
+  #Misclassification error for k nearest neighbours with cross-validation
+  #Parameters:
+  #weights: A numeric value used to rescale the binary features of the data (from column 11 to ncol-1)
+  #rawdata: The training data set with a first column of indexs and a last column with the labels
+  #cores: Integer for the number of cores used for parallel computing
+  #k: Integer for the number of nearest neighbours
+  #Values
+  #The function returns a numeric value for the misclassification error
+  
+  aux <- rawdata[,-c(1,30)] #remove unnecessary columns
+  columnsAux <- ncol(aux)
+  
+  #Weighting of the binary variables
+  aux[,11:(columnsAux-1)] <- aux[,11:(columnsAux-1)]*weights 
+  training <- aux #cleaned data set
+  
+  #Prepare data for parallel cross-validation
+  noBuckets <- 8 #number of buckets
+  noObs<-nrow(training)
+  idx <- rep(1:noBuckets, each=ceiling(nrow(training)/noBuckets))
+  idx <- idx[sample(1:noObs)] #Generate a random sample of indices
+  training <- data.frame(training,bucket=idx) #add column with bucket index
+  
+  BucketsList <- split(seq(1,noBuckets), seq(1,cores)) #Split buckets for parallelization
+  
+  #Parallel cross-validation
+  cl <- makeCluster(cores, type="SOCK", outfile="") 
+  registerDoSNOW(cl)  
+  
+  MisclassErrPar<- foreach(Buckets = BucketsList, .combine = rbind) %dopar% {
+    library(class)#install.packages("class")
+    MisclassErrBucket <- c() #Output variable per bucket
+    for (i in 1:length(Buckets)){    
+      cv<-Buckets[i] #Current out-of-sample bucket
+      #In-sample-data
+      traincol <- ncol(training)
+      Xtrain <- training[training$bucket != cv, -c(traincol-1, traincol)]  
+      Ytrain <- training[training$bucket != cv, (traincol-1)]
+      #Out-of-sample data
+      Xtest <- training[training$bucket == cv, -c(traincol-1, traincol)]
+      Ytest <- training[training$bucket == cv, (traincol-1)]
+      
+      # kNN classification
+      testPredictions <- knn(train=Xtrain, test=Xtest, cl=Ytrain, k=k)
+      MisclassErrBucket[i] <- mean (testPredictions != Ytest) #Misclassification Error for the bucket
+    }
+    MisclassErrBucket
+  }
+  stopCluster(cl)
+  
+  #Cross-validated misclassification error
+  MisclassErr <- mean(MisclassErrPar)
+  return(MisclassErr)
+}
--- Machine-LearningDLZ/Final_classifier/code/Optimal_kNN.R
+++ Machine-LearningDLZ/Final_classifier/code/Optimal_kNN.R
@@ -1,78 +1,76 @@
-#####Creates function to select optimal k for KNN
-
+########## Optimal prediction of forest covertypes using kNN ##########
+#Load required packages
 if (!require("foreach")) install.packages("foreach")
 if (!require("doSNOW")) install.packages("doSNOW")
 if (!require("class")) install.packages("class")
 if (!require("plyr")) install.packages("plyr")
 if (!require("dplyr")) install.packages("dplyr")
-#Load data
-rawdata <- read.csv("../data/Kaggle_Covertype_training.csv")
-rawTestdata <- read.csv("../data/Kaggle_Covertype_test.csv")
+source("CV_Optimalw.R") #Load in cross validation function for weights
+source("CV_Optimalk.R") #Load in cross validation function for k
+
+
+########### Loading the data ########### 
+#Load training and test sets
+rawdata <- read.csv("../../data/Kaggle_Covertype_training.csv") #Training
+rawTestdata <- read.csv("../../data/Kaggle_Covertype_test.csv") #Test
 numcol <- ncol(rawdata)
-numrow <- nrow(rawdata)
 rawdata[,numcol] <- factor(rawdata[,numcol]) #Classes as factors
 
-#for replication
-set.seed(10)
+########### Parameter optimization ########### 
+set.seed(10) #set seed for reproducibility
+ncores <- 8 #Number of cores
 
-#select optimal k  for NN
-#No rescaling of continuous features
+#k Optimization. No rescaling of continuous features
+ks<-c(1,3, 5, 7,9) #Number of NN
+#run function with cross validation to choose k
+results<-optimalK(rawdata, 
+                  10, 
+                  ks) #see CV_Optimalk.R
+optK<-aggregate(results$error[-1], list(results$k[-1]),mean)
+k<-optK[which.min(optK[,2]),1] #Optimal k
 
 
-optimalK<-function(training_data, buckets, ks) {
-    
-  #assigning obs to buckets for cross validation
-    noBuckets <- buckets
-    noObs<-nrow(training_data)
-    idx <- rep(1:noBuckets, each=ceiling(nrow(rawdata)/noBuckets)) 
-    idx <- idx[sample(1:noObs)]
-    
-    training <- training_data %>% mutate(bucket=idx) 
-    
-    training<- training %>% select( -soil_type_15)
-    
-    ks <- ks
-    
-    results<-data.frame(k=1,bucket=1,error=1)
-    
-          for (cv in 1:buckets) {
-            
-                #creating training data dependents
-                Xtrain <- training  %>% filter(bucket != cv) %>% select(-Cover_Type, -id, -bucket)
-                
-                #creating training data independent
-                Ytrain <- training %>% filter(bucket != cv) %>% select(Cover_Type)
-                
-                # subsetting the test phase data
-                
-                Xtest <- training %>% filter(bucket == cv) %>% select( -Cover_Type, -id, -bucket)
-                
-                Ytest <- training %>% filter(bucket == cv) %>% select( Cover_Type)
-                
-                  #Run kNN for each K and compute error
-                    for (k in 1:length(ks)) {
-                      # kNN results
-                      info<-paste(as.character(cv),as.character(k))
-                      testPredictions <- knn(train=Xtrain, test=Xtest, cl=Ytrain$Cover_Type, k=ks[k])
-                      totalError <-  mean(testPredictions != Ytest$Cover_Type)
-                      #track errors
-                      results<-rbind(results,c(k=ks[k],bucket=cv,error=totalError))
-              
-                    }
-        
-          }
-    return(results)
+#Optimization of weights with cross-validation
+MisclassErrOut <- c()
+for (i in 1:26){
+  weights <- 1+250*(i-1)/25 #Tested weights
+  MisclassErr <- Misclass1NN(weights = weights, 
+                             rawdata = rawdata, 
+                             cores = ncores, 
+                             k = k) #see CV_Parallel_kNN.R
+  cat("i=", i, " ",c(MisclassErr,weights), "\n")
+  MisclassErrOut <- rbind(MisclassErrOut, 
+                          c(MisclassErr,weights)) #Data frame of results
 }
 
-#k's to choose from
-ks<-c(1,3, 5, 7,9)
-
-#run function with cross validation to choose k
-results<-optimalK(rawdata, 10 , ks)
-
-#select optimal k with lowest mean error across buckets and k
-optK<-aggregate(results$error[-1], list(results$k[-1]),mean)
-
-k<-optK[which.min(optK[,2]),1] 
+#Save the misclassification errors for different weights
+#write.csv(MisclassErrOut, file = "1NN_OptimalWeight.csv", row.names=FALSE)
 
 
+########### Classification of Test set ########### 
+weights <- MisclassErrOut[which.min(MisclassErrOut[,1]),2] #Select the optimal weight
+
+#Build training and testing sets. It selects all the training set for training
+train <- rawdata[,-c(1,30)] #remove Ids and soil_type_15
+columnsAux <- ncol(train)
+
+#Training data
+train[,11:(columnsAux-1)] <- train[,11:(columnsAux-1)]*weights #weighted binary columns
+Xtrain <- train[,-columnsAux] #matrix of features
+Ytrain <- factor(train[,columnsAux]) #matrix of labels
+
+#Testing data
+Xtest <- rawTestdata[,-c(1,30)] #Remove Ids and soil_type_15. Only features on it
+Xtest[,11:(columnsAux-1)] <- Xtest[,11:(columnsAux-1)]*weights #weighted binary columns
+
+#Classification of the testing set
+testPredictions <- knn(train=Xtrain, 
+                       test=Xtest, 
+                       cl=Ytrain, 
+                       k=k)
+
+########### Saving the predicted covertypes ########### 
+#Save the predicted classes in the Kaggle format
+prediction<-data.frame(rawTestdata[,1],testPredictions)
+names(prediction) <- c("id", "Cover_Type")
+write.csv(prediction, file = "1NN_PredictionWeight.csv", row.names=FALSE)
